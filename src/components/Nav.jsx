ed473e469486dd4b23836ef7fd865ff1920e783e
RJ <rasajahromi@gmail.com>
2024-03-30 18:29:03 -0700

    added jersey

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -130,7 +130,7 @@ const Navbar = () => {
               <a href="https://www.offsideogs.com//">
                 <img src={mainLogo} alt="Main Logo" style={mainLogoStyles} /> {/* Use the main logo here */}
               </a>
-              <a href="https://www.instagram.com/itstexaco/" className="instagram-link">
+              <a href="https://www.instagram.com/offside.originals/" className="instagram-link">
               <img className="instagram-logo" src={instagramLogo} alt="Instagram" style={instagramStyles} />
             </a>
           </div>
